12f25e4fb4d4f44f0ce164068d8b109142bffb3b
Kramboja <koen5067@icloud.com>
2015-02-02 09:53:31 +0100

    Score + Time + Scene

---
+++ Assets/_Scripts/Score.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	private int _TimeRemaining;
+	private int _Score;
+
+	public void updateScore()
+	{
+		_TimeRemaining = GetComponent<TimeScript>().timeRemaining;// get how much time = remaining
+
+		Debug.Log (_TimeRemaining + " / " + _Score);
+		_Score = Mathf.FloorToInt (1000 - (10000f / _TimeRemaining));
+		Debug.Log (_TimeRemaining + " / " + _Score);
+
+	}
+
+	void Update () 
+	{
+		if(Input.GetKeyUp(KeyCode.A))
+		{
+			updateScore();
+		}
+	}
+}
